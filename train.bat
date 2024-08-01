b004bab8f24a58802b7e72df85ce835e31853f00
teigl <teiglandh@gmail.com>
2024-08-01 11:00:26 +0200

    stuff

--- train.bat
+++ train.bat
@@ -1,10 +1,11 @@
 python train_aux.py ^
   --workers 8 ^
   --device 0 ^
-  --batch-size 1 ^
+  --batch-size 2 ^
   --data data/wehak.yaml ^
   --img 1280 1280 ^
   --cfg cfg/training/yolov7-w6-wehak.yaml ^
-  --weights 'weights/yolov7-w6.pt' ^
+  --weights 'runs/train/yolov7-w6-wehak6/weights/best.pt' ^
   --name yolov7-w6-wehak ^
-  --hyp data/hyp.scratch.custom.yaml
+  --hyp data/hyp.scratch.custom.yaml ^
+  --epochs 3
